f626a50d0fde1960bbc2541d772b6311115f6435
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-10-31 18:27:34 +0100

    back

--- services/temperatureService.ts
+++ services/temperatureService.ts
@@ -9,3 +9,11 @@ export const convertTemperature = (value:string,unit:string) =>{
     if(isNaN(temp)) return ""
     return unit == UNITS.celcius ? Number(temp - 32/ 1.8).toFixed(2).toString() : (temp* 1.8 + 32).toFixed(2).toString()
 }
+
+export const isIceTemperature = (value:string,unit:string)=>{
+    const temp:number = Number(value) 
+    if(unit==UNITS.celcius){
+        return temp <= 0
+    }
+    else return temp <= 32
+}
